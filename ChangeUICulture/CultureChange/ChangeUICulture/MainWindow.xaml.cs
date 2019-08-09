ff0e13f4fab088d698d9acc2876964914aff0b20
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-08-09 22:38:38 +0300

    Create WFP Project to test change cuiture

---
+++ ChangeUICulture/CultureChange/ChangeUICulture/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Windows;
+
+namespace ChangeUICulture
+{
+    public partial class MainWindow : Window
+    {
+        DateTime displayDate = new DateTime(2014, 2, 28);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateUIElements();
+        }
+
+        private void UpdateUIElements()
+        {
+            CurrentCultureTextBlock.Text = string.Format(
+                Properties.Resources.CurrentCulture,
+                CultureInfo.CurrentCulture.ToString());
+            DateTextBlock.Text = displayDate.ToString("d");
+            LongDateTextBlock.Text = displayDate.ToString("D");
+        }
+
+        private void USButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.ChangeCulture(new CultureInfo("en-US"));
+        }
+
+        private void GBButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.ChangeCulture(new CultureInfo("en-GB"));
+        }
+
+        private void AUButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.ChangeCulture(new CultureInfo("en-AU"));
+        }
+
+        private void NZButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.ChangeCulture(new CultureInfo("en-NZ"));
+        }
+
+    }
+}
